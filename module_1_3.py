3ed6b06619f2c9d5864cda9fc477c6d9948e9190
sergeykuptsov84 <skuptsov1984@gmail.com>
2024-11-19 18:15:52 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = ('Sergey')  # variable str
+print('Name:' ,name)
+Age = 64             # variable int
+print('Age:', Age)
+Age = (Age+1)        # variable int
+print('New Age: ', Age)
+is_student = True  # variable bool
+print('Is Student:', is_student)
