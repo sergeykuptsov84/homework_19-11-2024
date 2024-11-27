588472beb467fd07cd268cbbbd31eed0035769f7
Sergey Kuptsov <skuptsov1984@gmail.com>
2024-11-27 19:25:16 +0300

    27/11_03 file pushed

--- module _2_02.py
+++ module _2_02.py
@@ -1,4 +1,3 @@
-from time import sleep
 print('Ведите три целых числа: ')
 first=(input('первое число: '))
 second=(input('второе число: '))
@@ -9,4 +8,3 @@ elif first == second or first == third or second == third:    # check second con
     print('2')
 else :                                                        # rest conditions
     print('0')
-sleep(5)
